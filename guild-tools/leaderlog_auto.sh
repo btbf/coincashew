6970fe22aae9a6d551c43449062ea51b5f1ffdf1
BTBF <b.t.b.f.corp@gmail.com>
2021-04-05 07:03:02 +0900

    Fixed

--- guild-tools/leaderlog_auto.sh
+++ guild-tools/leaderlog_auto.sh
@@ -4,11 +4,11 @@ tmux send-keys -t leaderlog C-c
 while true;do
 slot=$(curl -s localhost:12798/metrics | grep slotInEpoch | awk '{print $2}';sleep 3;)
 
-if [ $leader -eq 0 -a $slot -ge 365000 ]; then
+if [ $leader -eq 0 -a $slot -ge 320000 ]; then
 echo "$slot $leader"
 tmux send-keys -t leaderlog "~/cardano-my-node/scripts/cncli.sh leaderlog" Enter
 leader=1
-elif [ $leader -eq 1 -a $slot -ge 365000 ]; then
+elif [ $leader -eq 1 -a $slot -ge 320000 ]; then
 echo "$slot $leader"
 echo "スケジュール実行済み"
 else
